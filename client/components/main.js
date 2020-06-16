96fa113e0fcf20fb07f53c68a5f850cab106709c
gecuba <3dfash@gmail.com>
2020-06-17 00:04:16 +0300

    added id="search-button"

--- client/components/main.js
+++ client/components/main.js
@@ -19,9 +19,8 @@ const Main = () => {
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-black font-bold rounded-lg border shadow-lg p-10">
           <input type="text" id="input-field" value={value} onChange={onChange} className="mr-3" />
-          <button
+          <button type="button" id="search-button"
             className="bg-indigo-500 text-white font-bold rounded-lg border shadow-lg p-2"
-            type="button"
             onClick={onClick}
           >
             Search
